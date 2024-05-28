886334526562721b1699d9ca0829bae86abf9cfa
shubham <shubhamagarwal0632@gmail.com>
2024-05-28 16:40:50 +0530

    Initial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
